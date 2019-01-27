997ec5f00d85511c526fc91e74b9ae92edd431ae
AsiaKK <joanna.kaminska83@gmail.com>
2019-01-27 21:02:33 +0100

    first commit

---
+++ src/ShowCounts.java
@@ -0,0 +1,23 @@
+package src;
+
+public class ShowCounts {
+    public static void main(String[] args) {
+
+        System.out.println("Pętla while:");
+        double count = 0;
+        while (count <= 3) {
+            System.out.print(count);
+            System.out.print(", ");
+            count = count + 0.1;
+        }
+
+        System.out.println("\nPętla do while:");
+        count = 0;
+        do {
+            System.out.print(count);
+            System.out.print(", ");
+            count = count + 0.1;
+        }
+        while(count<=3);
+    }
+}
